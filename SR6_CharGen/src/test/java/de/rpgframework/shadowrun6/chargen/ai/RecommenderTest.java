d7697a0468d614570fdfea437d1dbaaa5665acb1
Stefan Prelle <stefan@rpgframework.de>
2022-01-06 16:57:47 +0100

    Use recommendation mechanics

---
+++ SR6_CharGen/src/test/java/de/rpgframework/shadowrun6/chargen/ai/RecommenderTest.java
@@ -0,0 +1,58 @@
+package de.rpgframework.shadowrun6.chargen.ai;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.rpgframework.MultiLanguageResourceBundle;
+import de.rpgframework.genericrpg.chargen.RecommendationState;
+import de.rpgframework.genericrpg.chargen.ai.AITool;
+import de.rpgframework.genericrpg.chargen.ai.Recommender;
+import de.rpgframework.shadowrun.MagicOrResonanceType;
+import de.rpgframework.shadowrun.ShadowrunAttribute;
+import de.rpgframework.shadowrun6.SR6MetaType;
+import de.rpgframework.shadowrun6.SR6Skill;
+import de.rpgframework.shadowrun6.Shadowrun6Character;
+import de.rpgframework.shadowrun6.Shadowrun6Core;
+import de.rpgframework.shadowrun6.chargen.gen.PointBuyAttributeGenerator;
+import de.rpgframework.shadowrun6.chargen.gen.PointBuyCharacterGenerator;
+import de.rpgframework.shadowrun6.chargen.gen.PointBuySkillGenerator;
+import de.rpgframework.shadowrun6.chargen.gen.SR6PointBuySettings;
+import de.rpgframework.shadowrun6.data.Shadowrun6DataPlugin;
+
+/**
+ * @author prelle
+ *
+ */
+public class RecommenderTest {
+
+	//-------------------------------------------------------------------
+	@BeforeClass
+	public static void setupClass() {
+		Shadowrun6DataPlugin plugin = new Shadowrun6DataPlugin();
+		plugin.init();
+	}
+
+	//-------------------------------------------------------------------
+	@Before
+	public void setup() {
+		MultiLanguageResourceBundle RES = new MultiLanguageResourceBundle(RecommenderTest.class.getPackageName()+".Recommender", Locale.ENGLISH);
+		AITool.loadProfiles(RecommenderTest.class.getResourceAsStream("profiles.xml"), RES);
+	}
+	
+	//-------------------------------------------------------------------
+	@Test
+	public void test1() {
+		Recommender recommender = new Recommender();
+		recommender.addConfiguration( AITool.getProfile("i_pick_every_lock").getModifications() );
+		
+		assertEquals(RecommendationState.RECOMMENDED, recommender.getRecommendationState(Shadowrun6Core.getSkill("engineering")));
+	}
+	
+}
