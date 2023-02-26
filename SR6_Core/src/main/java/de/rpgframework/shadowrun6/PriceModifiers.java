0a7e278aabf3618a934453a0217d41458c0941e0
Stefan Prelle <stefan@rpgframework.de>
2023-02-26 11:50:24 +0100

    COM6-214

---
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/PriceModifiers.java
@@ -0,0 +1,16 @@
+package de.rpgframework.shadowrun6;
+
+/**
+ * @author prelle
+ *
+ */
+public enum PriceModifiers {
+
+	EVERYTHING,
+	CLOTHING,
+	/** Clothing and Armor */
+	ARMOR,
+	LIFESTYLE_NECESSITIES
+
+
+}
--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/modifications/ShadowrunReference.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/modifications/ShadowrunReference.java
@@ -37,6 +37,7 @@ import de.rpgframework.shadowrun6.CreatePoints;
 import de.rpgframework.shadowrun6.DataStructure;
 import de.rpgframework.shadowrun6.LifepathModule;
 import de.rpgframework.shadowrun6.MartialArts;
+import de.rpgframework.shadowrun6.PriceModifiers;
 import de.rpgframework.shadowrun6.ReturnIdAsResultConverter;
 import de.rpgframework.shadowrun6.SR6MetaType;
 import de.rpgframework.shadowrun6.SR6NPC;
@@ -100,7 +101,8 @@ public enum ShadowrunReference implements ModifiedObjectType {
 	METAECHO(MetamagicOrEcho.class),
 	METATYPE(SR6MetaType.class),
 	PARAGON(MentorSpirit.class),
-	POOL("Pool"), // Derived values like defense pool
+	//POOL("Pool"), // Derived values like defense pool
+	PRICEMOD(PriceModifiers.class,0),
 	PROGRAM(ItemTemplate.class), //
 	// All Resistance tests
 	RESISTANCE(Resistance.class,0),
