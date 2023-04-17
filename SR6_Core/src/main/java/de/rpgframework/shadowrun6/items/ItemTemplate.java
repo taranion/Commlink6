799a05b3a9bb1f83c698200bbe49c27077896087
Stefan Prelle <stefan@rpgframework.de>
2023-04-17 13:47:17 +0200

    Fix translations

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/items/ItemTemplate.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/items/ItemTemplate.java
@@ -447,7 +447,9 @@ public class ItemTemplate extends PieceOfGear<SR6VariantMode,SR6UsageMode,SR6Pie
 			}
 			modifications.add(new ValueModification(ShadowrunReference.HOOK, ItemHook.VEHICLE_ACCESSORY.name(), 99));
 			modifications.add(new ValueModification(ShadowrunReference.HOOK, ItemHook.VEHICLE_CHASSIS.name(), body));
+			if (cf>0)
 			modifications.add(new ValueModification(ShadowrunReference.HOOK, ItemHook.VEHICLE_CF.name(), cf));
+			if (pt_slots>0)
 			modifications.add(new ValueModification(ShadowrunReference.HOOK, ItemHook.VEHICLE_POWERTRAIN.name(), pt_slots));
 			modifications.add(new ValueModification(ShadowrunReference.HOOK, ItemHook.VEHICLE_ELECTRONICS.name(), body));
 			break;
