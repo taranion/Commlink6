e35e49b4ed60742895c9313b761746fd055380b1
Stefan Prelle <stefan@rpgframework.de>
2023-06-29 20:04:20 +0200

    Remove unused code

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/DrakeType.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/DrakeType.java
@@ -1,10 +1,7 @@
 package de.rpgframework.shadowrun6;
 
-import java.util.List;
-
 import de.rpgframework.genericrpg.data.ComplexDataItem;
 import de.rpgframework.genericrpg.data.DataItemTypeKey;
-import de.rpgframework.shadowrun.ShadowrunAttribute;
 
 /**
  * @author prelle
