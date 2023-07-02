5ecb2f5acb5683af992f95fdf9e744365ae90655
Kid-Gen <rspahl@netcologne.de>
2023-07-02 09:55:41 +0200

    Update hack and slash code mods

--- SR6_Data/src/main/java/de/rpgframework/shadowrun6/data/Shadowrun6DataPlugin.java
+++ SR6_Data/src/main/java/de/rpgframework/shadowrun6/data/Shadowrun6DataPlugin.java
@@ -492,8 +492,8 @@ public class Shadowrun6DataPlugin  {
 		logger.log(Level.DEBUG, "Loaded "+list.size()+" vitual lifeform qualities");
 		list = Shadowrun6Core.loadDataItems(MetaTypeList.class, SR6MetaType.class, set, clazz, "hack_slash/data/metatypes.xml");
 		logger.log(Level.DEBUG, "Loaded "+list.size()+" AI types");
-//		list = Shadowrun6Core.loadDataItems(ItemTemplateList.class, ItemTemplate.class, set, clazz,"hack_slash/data/gear_codemods.xml");
-//		logger.log(Level.DEBUG, "Loaded "+list.size()+" codemods");
+		list = Shadowrun6Core.loadDataItems(ItemTemplateList.class, ItemTemplate.class, set, clazz,"hack_slash/data/gear_codemods.xml");
+		logger.log(Level.DEBUG, "Loaded "+list.size()+" codemods");
 		list = Shadowrun6Core.loadDataItems(MentorSpiritList.class, MentorSpirit.class, set, clazz,"hack_slash/data/mentorspirits.xml");
 		logger.log(Level.DEBUG, "Loaded "+list.size()+" paragons");
 		list = Shadowrun6Core.loadDataItems(QualityList.class, Quality.class, set, clazz, "hack_slash/data/qualities_streams.xml");
