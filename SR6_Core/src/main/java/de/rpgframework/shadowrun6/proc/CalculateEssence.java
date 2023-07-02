768284e781e41d50673d00cdbca9ab5a697f2bfd
Stefan Prelle <stefan@rpgframework.de>
2023-07-02 20:58:15 +0200

    COM6-369 Fix Magic/Resonance users with cyberware

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/CalculateEssence.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/CalculateEssence.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import de.rpgframework.character.ProcessingStep;
+import de.rpgframework.genericrpg.ValueType;
 import de.rpgframework.genericrpg.data.AttributeValue;
 import de.rpgframework.genericrpg.items.CarriedItem;
 import de.rpgframework.genericrpg.items.ItemAttributeFloatValue;
@@ -27,15 +28,15 @@ import de.rpgframework.shadowrun6.modifications.ShadowrunReference;
  */
 public class CalculateEssence implements ProcessingStep {
 
-	private final static Logger logger = System.getLogger(CalculateEssence.class.getPackageName());
-	
+	private final static Logger logger = System.getLogger(CalculateEssence.class.getPackageName()+".essence");
+
 	private Shadowrun6Character model;
 
 	//-------------------------------------------------------------------
 	public CalculateEssence(Shadowrun6Character model) {
 		this.model = model;
 	}
-	
+
 	//-------------------------------------------------------------------
 	@Override
 	public List<Modification> process(List<Modification> previous) {
@@ -52,13 +53,14 @@ public class CalculateEssence implements ProcessingStep {
 					ItemAttributeFloatValue<SR6ItemAttribute> aVal = item.getAsFloat(SR6ItemAttribute.ESSENCECOST);
 					logger.log(Level.DEBUG, "  essence = {0} for {1}",aVal, item.getKey());
 					if (aVal==null) continue;
-					double essence = aVal.getModifiedValueDouble(); 
+					double essence = aVal.getModifiedValueDouble();
 					logger.log(Level.INFO,"* "+item.getNameWithoutRating()+" = "+essence);
 					essenceCostBD = essenceCostBD.add(aVal.getModifiedValueBigDecimal());
 				}
 			}
 			int essenceCost = essenceCostBD.multiply(new BigDecimal(1000)).intValue();
-			
+			logger.log(Level.DEBUG, "Essence cost of all items {0}",essenceCost);
+
 			// Ensure presence of attributes
 			AttributeValue<ShadowrunAttribute> essVal = model.getAttribute(ShadowrunAttribute.ESSENCE);
 			if (essVal==null) {
@@ -71,13 +73,13 @@ public class CalculateEssence implements ProcessingStep {
 				holeVal = new AttributeValue<ShadowrunAttribute>(ShadowrunAttribute.ESSENCE_HOLE, 0);
 				model.setAttribute(holeVal);
 			}
-			
+
 			// Max essence to substract from is 6 plus additional essence hole
 			int max = 6000 - holeVal.getModifiedValue();
 			if (model.isInCareerMode() && model.getEssenceMaximum()>0) {
 				max = model.getEssenceMaximum() + holeVal.getModifiedValue();
 			}
-			
+
 			int newEssence = max - essenceCost;
 			// The new essence cannot be higher than the essence maximum
 			// If in theory it would, the difference is the new essence hole
@@ -91,24 +93,22 @@ public class CalculateEssence implements ProcessingStep {
 				essVal.setDistributed( newEssence );
 				model.setEssenceMaximum( newEssence);
 			}
-			
+
 			float remain = essVal.getModifiedValue() / 1000f;
 			logger.log(Level.DEBUG, "Essence cost is {0}, hole is {1}, resulting remain essence is {2}", essenceCost, holeVal.getModifiedValue(), remain);
 
-			double min = 6.0f - essenceCost; //Math.min(model.getEssence(), 6.0f-sum);
-
-			int magicMalus = 5 - (int)min;
+			double min = 6.0f - (((double)essenceCost)/1000f); //Math.min(model.getEssence(), 6.0f-sum);
+			int magicMalus = (int) Math.ceil( ((double)essenceCost)/1000d);
 			if (magicMalus<0) magicMalus=0;
-			logger.log(Level.INFO,"Magic malus is "+magicMalus);
+			logger.log(Level.DEBUG,"Magic/Resonance malus is "+magicMalus);
 			if (magicMalus!=0) {
 				model.getAttribute(ShadowrunAttribute.MAGIC).addModification(new ValueModification(ShadowrunReference.ATTRIBUTE, ShadowrunAttribute.MAGIC.name(), -magicMalus, ShadowrunAttribute.ESSENCE));
 				model.getAttribute(ShadowrunAttribute.RESONANCE).addModification(new ValueModification(ShadowrunReference.ATTRIBUTE, ShadowrunAttribute.RESONANCE.name(), -magicMalus, ShadowrunAttribute.ESSENCE));
 				model.getAttribute(ShadowrunAttribute.POWER_POINTS).addModification(new ValueModification(ShadowrunReference.ATTRIBUTE, ShadowrunAttribute.POWER_POINTS.name(), -magicMalus, ShadowrunAttribute.ESSENCE));
 			}
 			// Also decrease maximum
-//			model.getAttribute(Attribute.MAGIC).addModification(new AttributeModification(ModificationValueType.MAX, Attribute.MAGIC, -magicMalus, ModificationType.RELATIVE, Attribute.ESSENCE));
-//			previous.add(new AttributeModification(ModificationValueType.MAX, Attribute.MAGIC, -magicMalus, ModificationType.RELATIVE, Attribute.ESSENCE));
-			
+			model.getAttribute(ShadowrunAttribute.MAGIC).addModification(new ValueModification(ShadowrunReference.ATTRIBUTE, ShadowrunAttribute.MAGIC.name(), -magicMalus, ShadowrunAttribute.ESSENCE, ValueType.MAX));
+
 		} finally {
 			logger.log(Level.TRACE, "LEAVE: process");
 		}
