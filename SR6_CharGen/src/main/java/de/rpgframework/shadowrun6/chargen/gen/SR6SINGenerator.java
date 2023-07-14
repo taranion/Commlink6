5e69c8d8734ee374fdf74122357da7594bf737b0
Stefan Prelle <stefan@rpgframework.de>
2023-07-14 19:57:48 +0200

    Fix dealing with REAL sins

--- SR6_CharGen/src/main/java/de/rpgframework/shadowrun6/chargen/gen/SR6SINGenerator.java
+++ SR6_CharGen/src/main/java/de/rpgframework/shadowrun6/chargen/gen/SR6SINGenerator.java
@@ -55,46 +55,27 @@ public class SR6SINGenerator extends CommonSINController {
 				}
 			}
 
-			boolean autoAddedRealSIN = false;
 			for (Modification tmp : previous) {
-				if (tmp.getReferenceType()==ShadowrunReference.SIN) {
-					DataItemModification mod = (DataItemModification)tmp;
-					logger.log(Level.DEBUG, "process "+mod);
-					FakeRating qual = mod.getResolvedKey();
-					
-					// Re-use an eventually existing REAL_SIN
-					if (qual==FakeRating.REAL_SIN) {
-						autoAddedRealSIN = true;
-						if (real==null) {
-							real = new SIN(qual);
-							real.setName(getModel().getName());
-							logger.log(Level.INFO, "add REAL sin");
-							getModel().addSIN(real);
-						} else {
-							logger.log(Level.INFO, "keep existing REAL sin");
-						}
-					}
-				} else
-					unprocessed.add(tmp);
+				unprocessed.add(tmp);
 			}
-			
+
 			// If a real SIN exists, but isn't auto-added anymore, remove it
-			if (real!=null && !autoAddedRealSIN) {
+			if (real!=null && real.getInjectedBy()==null) {
 				getModel().removeSIN(real);
 				logger.log(Level.INFO, "removed REAL sin");
 			}
-			
+
 			// Now pay SINs
 			for (SIN sin : getModel().getSINs()) {
-				if (sin.getQuality()==FakeRating.REAL_SIN) 
+				if (sin.getQuality()==FakeRating.REAL_SIN)
 					continue;
 				int cost = sin.getQualityValue() * 2500;
 				logger.log(Level.INFO, "Pay {0} nuyen for rating {1} SIN: {2}", cost, sin.getQualityValue(), sin.getName());
 				getModel().setNuyen( getModel().getNuyen() - cost);
 			}
-			
+
 		} finally {
-			if (logger.isLoggable(Level.TRACE)) logger.log(Level.TRACE, "LEAVE process");			
+			if (logger.isLoggable(Level.TRACE)) logger.log(Level.TRACE, "LEAVE process");
 		}
 		return unprocessed;
 	}
