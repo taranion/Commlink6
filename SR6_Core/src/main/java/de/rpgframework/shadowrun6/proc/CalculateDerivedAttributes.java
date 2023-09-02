0e6ea892cee76b41dc6c9cac0cc4f862652a1dc8
Stefan Prelle <stefan@rpgframework.de>
2023-09-02 23:24:23 +0200

    COM6527 Fix "In Debt"

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/CalculateDerivedAttributes.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/CalculateDerivedAttributes.java
@@ -17,9 +17,11 @@ import de.rpgframework.shadowrun.BodyForm;
 import de.rpgframework.shadowrun.BodyType;
 import de.rpgframework.shadowrun.DamageType;
 import de.rpgframework.shadowrun.Lifestyle;
+import de.rpgframework.shadowrun.Quality;
 import de.rpgframework.shadowrun.ShadowrunAttribute;
 import de.rpgframework.shadowrun6.SR6RuleFlag;
 import de.rpgframework.shadowrun6.Shadowrun6Character;
+import de.rpgframework.shadowrun6.Shadowrun6Core;
 import de.rpgframework.shadowrun6.Shadowrun6Tools;
 import de.rpgframework.shadowrun6.items.ItemTemplate;
 import de.rpgframework.shadowrun6.items.SR6ItemAttribute;
@@ -256,6 +258,12 @@ public class CalculateDerivedAttributes implements ProcessingStep {
 			int toPay = lifestyle.getCostPerMonth();
 			addNaturalModifier(val, toPay, lifestyle);
 		}
+
+		// In Debt
+		if (model.getDebtRate()>0) {
+			addNaturalModifier(val, model.getDebtRate(), Shadowrun6Core.getItem(Quality.class, "in_debt"));
+		}
+
 		logger.log(Level.DEBUG, " Monthly cost   = "+val.getModifiedValue());
 	}
 
