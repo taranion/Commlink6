6c9d47326ffa2d4c8cc890fda0f0c225848f1081
Stefan Prelle <stefan@rpgframework.de>
2023-10-08 16:57:10 +0200

    Clean up unused

--- SR6_CharGen/src/main/java/de/rpgframework/shadowrun6/chargen/gen/free/SR6FreeEquipmentGenerator.java
+++ SR6_CharGen/src/main/java/de/rpgframework/shadowrun6/chargen/gen/free/SR6FreeEquipmentGenerator.java
@@ -1,23 +1,11 @@
 package de.rpgframework.shadowrun6.chargen.gen.free;
 
 import de.rpgframework.genericrpg.Possible;
-import de.rpgframework.genericrpg.ToDoElement.Severity;
-import de.rpgframework.genericrpg.chargen.OperationResult;
 import de.rpgframework.genericrpg.data.Decision;
-import de.rpgframework.genericrpg.items.CarriedItem;
 import de.rpgframework.genericrpg.items.CarryMode;
-import de.rpgframework.genericrpg.items.GearTool;
-import de.rpgframework.genericrpg.items.PieceOfGearVariant;
-import de.rpgframework.shadowrun.chargen.charctrl.IRejectReasons;
-import de.rpgframework.shadowrun.items.Availability;
-import de.rpgframework.shadowrun.items.Legality;
-import de.rpgframework.shadowrun6.Shadowrun6Rules;
 import de.rpgframework.shadowrun6.chargen.charctrl.SR6CharacterController;
-import de.rpgframework.shadowrun6.chargen.charctrl.SR6CharacterGenerator;
 import de.rpgframework.shadowrun6.chargen.gen.CommonEquipmentGenerator;
 import de.rpgframework.shadowrun6.items.ItemTemplate;
-import de.rpgframework.shadowrun6.items.SR6ItemAttribute;
-import de.rpgframework.shadowrun6.items.SR6VariantMode;
 
 /**
  * @author prelle
