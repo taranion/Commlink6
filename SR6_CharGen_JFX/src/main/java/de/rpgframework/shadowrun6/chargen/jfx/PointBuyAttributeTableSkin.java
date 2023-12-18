e6a7c305c72c8982eacbe0b6115e75199c3f507a
Stefan Prelle <stefan@rpgframework.de>
2023-12-18 16:30:48 +0100

    Reduce debug

--- SR6_CharGen_JFX/src/main/java/de/rpgframework/shadowrun6/chargen/jfx/PointBuyAttributeTableSkin.java
+++ SR6_CharGen_JFX/src/main/java/de/rpgframework/shadowrun6/chargen/jfx/PointBuyAttributeTableSkin.java
@@ -625,7 +625,6 @@ public class PointBuyAttributeTableSkin extends SkinBase<PointBuyAttributeTable<
 //			lblAll.get(key).setText(String.valueOf(val.getDistributed()));
 			lblAll.get(key).setText(val.getPool().toString());
 			lblAll.get(key).setTooltip(new Tooltip(val.getPool().toExplainString()));
-			System.err.println("PointBuyAttributeTableSkin: "+key+" = "+val.getPool().toExplainString());
 			if (lblAdj.get(key)!=null) {
 				lblAdj.get(key).setText(String.valueOf(per.points1));
 				if (toggles.getSelectedToggle()==headBtnAdjust) {
