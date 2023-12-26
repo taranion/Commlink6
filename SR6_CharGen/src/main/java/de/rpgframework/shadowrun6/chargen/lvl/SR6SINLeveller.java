64a03ea6bc7dd97dff3e7c12618d3b2d32da21eb
Stefan Prelle <stefan@rpgframework.de>
2023-12-26 10:38:51 +0100

    COM6-642 COMMBUG-255 Fix real SIN in career

--- SR6_CharGen/src/main/java/de/rpgframework/shadowrun6/chargen/lvl/SR6SINLeveller.java
+++ SR6_CharGen/src/main/java/de/rpgframework/shadowrun6/chargen/lvl/SR6SINLeveller.java
@@ -2,10 +2,8 @@ package de.rpgframework.shadowrun6.chargen.lvl;
 
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
-import java.util.ArrayList;
 import java.util.List;
 
-import de.rpgframework.genericrpg.modification.DataItemModification;
 import de.rpgframework.genericrpg.modification.Modification;
 import de.rpgframework.shadowrun.LicenseValue;
 import de.rpgframework.shadowrun.SIN;
@@ -13,7 +11,6 @@ import de.rpgframework.shadowrun.SIN.FakeRating;
 import de.rpgframework.shadowrun.ShadowrunRules;
 import de.rpgframework.shadowrun6.chargen.charctrl.CommonSINController;
 import de.rpgframework.shadowrun6.chargen.charctrl.SR6CharacterController;
-import de.rpgframework.shadowrun6.modifications.ShadowrunReference;
 
 /**
  * @author prelle
@@ -39,47 +36,26 @@ public class SR6SINLeveller extends CommonSINController {
 	 * @see de.rpgframework.character.ProcessingStep#process(java.util.List)
 	 */
 	@Override
-	public List<Modification> process(List<Modification> previous) {
+	public List<Modification> process(List<Modification> unprocessed) {
 		if (logger.isLoggable(Level.TRACE)) logger.log(Level.TRACE, "ENTER process");
-		List<Modification> unprocessed = new ArrayList<>();
+
 		try {
 			todos.clear();
+			boolean autoAddedRealSIN = false;
 			// Search for an eventually existing real SIN
 			// it will be deleted, if there is no modification for it
 			SIN real = null;
 			for (SIN s : getModel().getSINs()) {
 				if (s.getQuality()==FakeRating.REAL_SIN) {
+					if (s.getInjectedBy()!=null) autoAddedRealSIN = true;
 					real = s; break;
 				}
-			}
-
-			boolean autoAddedRealSIN = false;
-			for (Modification tmp : previous) {
-				if (tmp.getReferenceType()==ShadowrunReference.SIN) {
-					DataItemModification mod = (DataItemModification)tmp;
-					logger.log(Level.DEBUG, "process "+mod);
-					FakeRating qual = mod.getResolvedKey();
-
-					// Re-use an eventually existing REAL_SIN
-					if (qual==FakeRating.REAL_SIN) {
-						autoAddedRealSIN = true;
-						if (real==null) {
-							real = new SIN(qual);
-							real.setName(getModel().getName());
-							logger.log(Level.INFO, "add REAL sin");
-							getModel().addSIN(real);
-						} else {
-							logger.log(Level.INFO, "keep existing REAL sin");
-						}
-					}
-				} else
-					unprocessed.add(tmp);
 			}
 
 			// If a real SIN exists, but isn't auto-added anymore, remove it
 			if (real!=null && !autoAddedRealSIN) {
 				getModel().removeSIN(real);
-				logger.log(Level.INFO, "removed REAL sin");
+				logger.log(Level.WARNING, "removed REAL sin");
 			}
 
 		} finally {
