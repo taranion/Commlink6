1dd8e1eab2452de342eb1a6655da5775ccabff63
Stefan Prelle <stefan@rpgframework.de>
2024-05-03 15:31:26 +0200

    COM6-732

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/CalculateSkillPools.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/CalculateSkillPools.java
@@ -57,24 +57,21 @@ public class CalculateSkillPools implements ProcessingStep {
 		Pool<Integer> pool = new Pool<Integer>();
 		sVal.setPool(pool);
 
-		// Clone pool for attribute, if possible
+		// Determine attribute
 		String memArtName = null;
 		int memArtVal  = 0;
 		ShadowrunAttribute attr = sVal.getSkill().getAttribute();
 		if (attr!=null) {
-//			pool = (Pool<Integer>) model.getAttribute(attr).getPool().clone();
 			AttributeValue<ShadowrunAttribute> aVal = model.getAttribute(attr);
-			pool.addStep(ValueType.NATURAL, new PoolCalculation<Integer>(
-					Math.max(aVal.getModifiedValue(ValueType.ARTIFICIAL), aVal.getModifiedValue(ValueType.AUGMENTED)),
-					attr.getName(loc)));
 			memArtName = attr.getName(loc);
-			memArtVal = Math.max(aVal.getModifiedValue(ValueType.ARTIFICIAL), aVal.getModifiedValue(ValueType.AUGMENTED));
+			// Use either augmented natural or artificial value - whatever is better
+			memArtVal = Math.max(aVal.getModifiedValue(ValueType.ARTIFICIAL), aVal.getModifiedValue(ValueType.NATURAL, ValueType.AUGMENTED));
+			pool.addStep(ValueType.NATURAL, new PoolCalculation<Integer>(memArtVal,memArtName));
 		}
 
 		int augmentedMax = sVal.getDistributed() + 4;
-
 		/*
-		 * Natural attribute first
+		 * Natural skill first
 		 */
 		pool.addStep(ValueType.NATURAL, new PoolCalculation<Integer>(sVal.getDistributed(), sVal.getModifyable().getName(loc)));
 		// Add all natural modifiers
