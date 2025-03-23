e61e30caa687095b6939f0285295de688dffd3ea
Stefan Prelle <stefan@rpgframework.de>
2025-03-23 22:19:42 +0100

    Better deal with UUIDs for EssenceChange

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/Shadowrun6Tools.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/Shadowrun6Tools.java
@@ -2732,23 +2732,23 @@ public class Shadowrun6Tools {
 	}
 
 	//-------------------------------------------------------------------
-	public static int recordEssenceChange(Shadowrun6Character model, ComplexDataItemValue<?> value) {
+	public static ValueModification recordEssenceChange(Shadowrun6Character model, ComplexDataItemValue<?> value) {
 		ValueModification mod = toValueMod(model, value);
 		if (mod!=null) {
 			model.getEssenceChanges().add(mod);
-			return mod.getValue();
+			return mod;
 		}
-		return 0;
+		return null;
 	}
 
 	//-------------------------------------------------------------------
-	public static int recordEssenceChange(Shadowrun6Character model, ComplexDataItem value) {
+	public static ValueModification recordEssenceChange(Shadowrun6Character model, ComplexDataItem value) {
 		ValueModification mod = toValueMod(model, value);
 		if (mod!=null) {
 			model.getEssenceChanges().add(mod);
-			return mod.getValue();
+			return mod;
 		}
-		return 0;
+		return null;
 	}
 
 	//-------------------------------------------------------------------
@@ -2757,7 +2757,7 @@ public class Shadowrun6Tools {
 		try {
 		ValueModification toRemove = null;
 		for (ValueModification vMod : model.getEssenceChanges()) {
-			if (vMod.getId().equals(value.getUuid())) {
+			if (vMod.getId()!=null && vMod.getId().equals(value.getUuid())) {
 				toRemove = vMod;
 				break;
 			}
@@ -2786,6 +2786,10 @@ public class Shadowrun6Tools {
 	public static void removeEssenceChange(Shadowrun6Character model, ComplexDataItem value, RemoveMode mode) {
 		logger.log(Level.WARNING, "ENTER removeEssenceChange( {1}, {0} )", mode, value);
 		try {
+			model.getEssenceChanges().stream()
+				.filter(vMod -> vMod.getKey().equals(value.getId()))
+				.forEach(mod -> model.getEssenceChanges().remove(mod));
+			
 		ValueModification toRemove = null;
 		for (ValueModification vMod : model.getEssenceChanges()) {
 			if (vMod.getKey().equals(value.getId())) {
