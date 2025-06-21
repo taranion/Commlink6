817ad4aeda26e5ec2cf93e2a76755e896f143980
Stefan Prelle <stefan@rpgframework.de>
2025-06-21 14:58:25 +0200

    COM6-875

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/FixCharacterStep.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/FixCharacterStep.java
@@ -1,27 +1,20 @@
 package de.rpgframework.shadowrun6.proc;
 
 import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 import de.rpgframework.character.ProcessingStep;
-import de.rpgframework.genericrpg.chargen.OperationResult;
+import de.rpgframework.core.BabylonEventBus;
+import de.rpgframework.core.BabylonEventListener;
+import de.rpgframework.core.BabylonEventType;
 import de.rpgframework.genericrpg.items.CarriedItem;
-import de.rpgframework.genericrpg.items.CarryMode;
-import de.rpgframework.genericrpg.items.ItemAttributeNumericalValue;
-import de.rpgframework.genericrpg.items.ItemAttributeObjectValue;
 import de.rpgframework.genericrpg.modification.Modification;
+import de.rpgframework.shadowrun.LicenseValue;
+import de.rpgframework.shadowrun.SIN;
 import de.rpgframework.shadowrun6.Shadowrun6Character;
-import de.rpgframework.shadowrun6.items.AvailableSlot;
-import de.rpgframework.shadowrun6.items.ItemHook;
-import de.rpgframework.shadowrun6.items.ItemSubType;
 import de.rpgframework.shadowrun6.items.ItemTemplate;
-import de.rpgframework.shadowrun6.items.ItemType;
-import de.rpgframework.shadowrun6.items.ItemUtil;
-import de.rpgframework.shadowrun6.items.SR6GearTool;
-import de.rpgframework.shadowrun6.items.SR6ItemAttribute;
-import de.rpgframework.shadowrun6.items.SR6ItemFlag;
 
 /**
  * Clean up character from old mistakes / design decisions
@@ -53,7 +46,28 @@ public class FixCharacterStep implements ProcessingStep {
 			item.clearEmptyFlags();
 		}
 
+		fixLicencesWithoutSIN();
+		
 		return unprocessed;
+	}
+
+	//-------------------------------------------------------------------
+	private void fixLicencesWithoutSIN() {
+		Optional<SIN> sin = model.getSINs().stream().findFirst();
+		List<String> mess = new ArrayList<>();
+		if (sin.isPresent()) {
+			for (LicenseValue lic : model.getLicenses()) {
+				if (lic.getSIN()==null) {
+					lic.setSIN(sin.get().getUniqueId());
+					mess.add(String.format("SIN-less license %s assigned to SIN %s", lic.getName(), sin.get().getName()));
+				}
+			}
+		}
+		if (!mess.isEmpty()) {
+			mess.add(0, "Character "+model.getName());
+			BabylonEventBus.fireEvent(BabylonEventType.UI_MESSAGE, 0, String.join("\r\n", mess));
+			mess.add("\nPlease verify character and save it again");
+		}
 	}
 
 }
