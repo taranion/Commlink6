8014805a83a3fab4cc6d2c527136ed7d495778e2
Stefan Prelle <stefan@rpgframework.de>
2025-07-20 21:09:48 +0200

    COM6-902

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/FixCharacterStep.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/FixCharacterStep.java
@@ -9,12 +9,17 @@ import de.rpgframework.character.ProcessingStep;
 import de.rpgframework.core.BabylonEventBus;
 import de.rpgframework.core.BabylonEventListener;
 import de.rpgframework.core.BabylonEventType;
+import de.rpgframework.genericrpg.Reward;
+import de.rpgframework.genericrpg.data.AttributeValue;
 import de.rpgframework.genericrpg.items.CarriedItem;
 import de.rpgframework.genericrpg.modification.Modification;
+import de.rpgframework.genericrpg.modification.ValueModification;
 import de.rpgframework.shadowrun.LicenseValue;
 import de.rpgframework.shadowrun.SIN;
+import de.rpgframework.shadowrun.ShadowrunAttribute;
 import de.rpgframework.shadowrun6.Shadowrun6Character;
 import de.rpgframework.shadowrun6.items.ItemTemplate;
+import de.rpgframework.shadowrun6.modifications.ShadowrunReference;
 
 /**
  * Clean up character from old mistakes / design decisions
@@ -47,6 +52,7 @@ public class FixCharacterStep implements ProcessingStep {
 		}
 
 		fixLicencesWithoutSIN();
+		fixMissingHeat();
 		
 		return unprocessed;
 	}
@@ -67,6 +73,30 @@ public class FixCharacterStep implements ProcessingStep {
 			mess.add(0, "Character "+model.getName());
 			mess.add("\nPlease verify character and save it again");
 			BabylonEventBus.fireEvent(BabylonEventType.UI_MESSAGE, 0, String.join("\r\n", mess));
+		}
+	}
+	
+	//-------------------------------------------------------------------
+	private void fixMissingHeat() {
+		// Calculate heat, if it doesn't exit
+		int heat = 0;
+		AttributeValue<ShadowrunAttribute> heatVal = model.getAttribute(ShadowrunAttribute.HEAT);
+		if (heatVal==null) {
+			heatVal = new AttributeValue<ShadowrunAttribute>(ShadowrunAttribute.HEAT);
+			model.setAttribute(heatVal);
+			try {
+				for (Reward reward : model.getRewards()) {
+					ValueModification modHeat = reward.getModification(ShadowrunReference.ATTRIBUTE, ShadowrunAttribute.HEAT.name());
+					if (modHeat!=null) {
+						heat += modHeat.getValue();
+						if (heat<0) heat=0;
+					}
+				}
+				model.getAttribute(ShadowrunAttribute.HEAT).setDistributed(heat);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
