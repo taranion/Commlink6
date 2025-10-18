dc4473618adf55ab99bae173b135ae039bd4bda2
Stefan Prelle <stefan@rpgframework.de>
2025-10-18 21:07:08 +0200

    Improve drake weapon situation

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/GetModificationsForDrakes.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/proc/GetModificationsForDrakes.java
@@ -1,11 +1,26 @@
 package de.rpgframework.shadowrun6.proc;
 
+import static de.rpgframework.shadowrun.ShadowrunAttribute.AGILITY;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.BODY;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.CHARISMA;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.DRAKE_EDGE;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.EDGE;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.INTUITION;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.LOGIC;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.MAGIC;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.REACTION;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.RESONANCE;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.STRENGTH;
+import static de.rpgframework.shadowrun.ShadowrunAttribute.WILLPOWER;
+
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 import de.rpgframework.character.ProcessingStep;
+import de.rpgframework.genericrpg.Pool;
 import de.rpgframework.genericrpg.ValueType;
 import de.rpgframework.genericrpg.data.AttributeValue;
 import de.rpgframework.genericrpg.items.CarriedItem;
@@ -19,6 +34,8 @@ import de.rpgframework.shadowrun.BodyForm;
 import de.rpgframework.shadowrun.BodyType;
 import de.rpgframework.shadowrun.CritterPower;
 import de.rpgframework.shadowrun.CritterPowerValue;
+import de.rpgframework.shadowrun.DamageElement;
+import de.rpgframework.shadowrun.DamageType;
 import de.rpgframework.shadowrun.MetamagicOrEcho;
 import de.rpgframework.shadowrun.MetamagicOrEcho.Type;
 import de.rpgframework.shadowrun.MetamagicOrEchoValue;
@@ -29,11 +46,11 @@ import de.rpgframework.shadowrun.QualityValue;
 import de.rpgframework.shadowrun.ShadowrunAttribute;
 import de.rpgframework.shadowrun6.DrakeTypeValue;
 import de.rpgframework.shadowrun6.Shadowrun6Character;
+import de.rpgframework.shadowrun6.Shadowrun6Core;
+import de.rpgframework.shadowrun6.items.Damage;
 import de.rpgframework.shadowrun6.items.ItemTemplate;
 import de.rpgframework.shadowrun6.items.SR6ItemAttribute;
 import de.rpgframework.shadowrun6.modifications.ShadowrunReference;
-
-import static de.rpgframework.shadowrun.ShadowrunAttribute.*;
 
 /**
  * @author prelle
@@ -75,7 +92,7 @@ public class GetModificationsForDrakes implements ProcessingStep {
 
 			// Copy attributes from regular body to drake body
 			ShadowrunAttribute[] attributes = new ShadowrunAttribute[]{BODY,AGILITY,REACTION,STRENGTH, WILLPOWER,LOGIC,INTUITION,CHARISMA,EDGE,MAGIC,RESONANCE,DRAKE_EDGE};
-			for (ShadowrunAttribute key : attributes) {
+			for (ShadowrunAttribute key : ShadowrunAttribute.primaryAndSpecialValues()) {
 				AttributeValue<ShadowrunAttribute> aVal = model.getAttribute(key);
 				AttributeValue<ShadowrunAttribute> copy = new AttributeValue<ShadowrunAttribute>(key, aVal.getDistributed());
 				body.getAttributeValues().add(copy);
@@ -83,7 +100,10 @@ public class GetModificationsForDrakes implements ProcessingStep {
 
 			// Calculate DRAKE_EDGE
 			int dEdge = model.getAttribute(ShadowrunAttribute.EDGE).getModifiedValue();
-			dEdge += model.getMetamagicOrEchoes().stream().filter(me -> me.getModifyable().getType()==Type.DRACOGENESIS_POWER).count();
+			dEdge += model.getMetamagicOrEchoes().stream()
+					.filter(me -> me.getModifyable().getType()==Type.DRACOGENESIS_POWER)
+					.filter(me -> !me.isAutoAdded())
+					.count();
 			body.getAttributeValues().add(new AttributeValue<ShadowrunAttribute>(ShadowrunAttribute.DRAKE_EDGE, dEdge));
 
 			// Modify with drake adjustments
@@ -100,11 +120,40 @@ public class GetModificationsForDrakes implements ProcessingStep {
 			
 			// Add natural weapon
 //			body.clearWeapons();
+			
+			// Natural weapon
+			Supplier<Pool<Integer>> supp = () -> {
+				Pool<Integer> pool = new Pool<>();
+				return pool;
+			};
 			ItemTemplate natural = new ItemTemplate();
-			natural.setId("drake.naturalweapon");
-			CarriedItem<ItemTemplate> elementalAttack = new CarriedItem<ItemTemplate>(natural, null, CarryMode.VIRTUAL);
-			ItemAttributeObjectValue<SR6ItemAttribute> av = new ItemAttributeObjectValue<>(SR6ItemAttribute.ATTACK_RATING, new int[] {dEdge*2, dEdge*2-2, dEdge*2-4, dEdge*2-6, dEdge*2-8, dEdge*2-10});
+			natural.setId("claws");
+			Shadowrun6Core.getDataSet("LOFWYR").ifPresent(ds -> natural.assignToDataSet(ds));
+			
+			CarriedItem<ItemTemplate> natAttack = new CarriedItem<ItemTemplate>(natural, null, CarryMode.VIRTUAL);
+			int strPlusRea = body.getAttributeValue(ShadowrunAttribute.STRENGTH).getModifiedValue()
+					+ body.getAttributeValue(ShadowrunAttribute.REACTION).getModifiedValue();
+			int dmg = (int) Math.round( (double)body.getAttributeValue(ShadowrunAttribute.STRENGTH).getModifiedValue() / 2.0);
+			ItemAttributeObjectValue<SR6ItemAttribute> av = new ItemAttributeObjectValue<>(SR6ItemAttribute.ATTACK_RATING, new int[] {strPlusRea,0,0,0,0});
+			natAttack.setAttribute(SR6ItemAttribute.ATTACK_RATING, av);
+			natAttack.setAttribute(SR6ItemAttribute.DAMAGE, new ItemAttributeObjectValue<SR6ItemAttribute>(SR6ItemAttribute.DAMAGE, new Damage(dmg, DamageType.PHYSICAL, DamageElement.REGULAR)));
+			natAttack.setAttribute(SR6ItemAttribute.POOL_SUPPLIER, new ItemAttributeObjectValue<SR6ItemAttribute>(SR6ItemAttribute.DAMAGE, supp));
+			body.addNaturalWeapon(natAttack);
+			
+			// Elemental attack
+			supp = () -> {
+				Pool<Integer> pool = new Pool<>();
+				return pool;
+			};
+			ItemTemplate elemAt = new ItemTemplate();
+			elemAt.setId("elemental_attack");
+			Shadowrun6Core.getDataSet("LOFWYR").ifPresent(ds -> elemAt.assignToDataSet(ds));
+			
+			CarriedItem<ItemTemplate> elementalAttack = new CarriedItem<ItemTemplate>(elemAt, null, CarryMode.VIRTUAL);
+			av = new ItemAttributeObjectValue<>(SR6ItemAttribute.ATTACK_RATING, new int[] {dEdge*2, dEdge*2-2, dEdge*2-8, dEdge*2-10,0});
 			elementalAttack.setAttribute(SR6ItemAttribute.ATTACK_RATING, av);
+			elementalAttack.setAttribute(SR6ItemAttribute.DAMAGE, new ItemAttributeObjectValue<SR6ItemAttribute>(SR6ItemAttribute.DAMAGE, new Damage(dEdge, DamageType.PHYSICAL, DamageElement.FIRE)));
+			elementalAttack.setAttribute(SR6ItemAttribute.POOL_SUPPLIER, new ItemAttributeObjectValue<SR6ItemAttribute>(SR6ItemAttribute.DAMAGE, supp));
 			body.addNaturalWeapon(elementalAttack);
 
 
