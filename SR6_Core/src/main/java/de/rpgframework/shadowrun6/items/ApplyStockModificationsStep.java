164e53f67bd21919ba3df58fbc0b8a566a0ef18b
Stefan Prelle <stefan@rpgframework.de>
2026-02-14 12:23:51 +0100

    partially undo COM6-971

--- SR6_Core/src/main/java/de/rpgframework/shadowrun6/items/ApplyStockModificationsStep.java
+++ SR6_Core/src/main/java/de/rpgframework/shadowrun6/items/ApplyStockModificationsStep.java
@@ -14,8 +14,6 @@ import de.rpgframework.genericrpg.items.CarriedItem;
 import de.rpgframework.genericrpg.items.CarriedItemProcessor;
 import de.rpgframework.genericrpg.items.CarryMode;
 import de.rpgframework.genericrpg.items.ItemEnhancementValue;
-import de.rpgframework.genericrpg.items.PieceOfGear;
-import de.rpgframework.genericrpg.items.PieceOfGearVariant;
 import de.rpgframework.genericrpg.modification.DataItemModification;
 import de.rpgframework.genericrpg.modification.EmbedModification;
 import de.rpgframework.genericrpg.modification.Modification;
@@ -240,15 +238,9 @@ public class ApplyStockModificationsStep implements CarriedItemProcessor {
 					return true;
 				}
 			}
-			// Is there a variant given?
-			SR6PieceOfGearVariant variant = null;
-			if (mod.getVariant()!=null) {
-				variant = templ.getVariant(mod.getVariant());
-			}
-
 			ItemHook hook = mod.getHook();
 			logger.log(Level.INFO, "Add instanceof {0} into hook {1} of {2} - with {3} decisions", templ.getId(), hook, model.getKey(), decs.length);
-			OperationResult<CarriedItem<ItemTemplate>> carriedR = SR6GearTool.buildItem(templ, CarryMode.EMBEDDED, variant, charac, false, model, decs);
+			OperationResult<CarriedItem<ItemTemplate>> carriedR = SR6GearTool.buildItem(templ, CarryMode.EMBEDDED, null, charac, false, model, decs);
 			if (carriedR.hasError()) {
 //				logger.log(Level.ERROR, "Error embedding {0} into hook {1} of {2}: {3}", mod.getKey(), hook, model.getKey(),carriedR.getError());
 				return true;
@@ -259,35 +251,20 @@ public class ApplyStockModificationsStep implements CarriedItemProcessor {
 			if (mod.getId()!=null) {
 				accessory.setUuid(mod.getId());
 			}
-
-			// Check if AvailableSlot already exists - if not, create one
-//			AvailableSlot slot = (AvailableSlot) model.getSlot(hook);
-//			if (slot==null) {
-//				if (hook.hasCapacity && strict) {
-//					logger.log(Level.WARNING, "Item {0} (from {1}) has an <embed> for a not existing slot {2}, but cannot auto-create slot since no capacity is given",
-//							mod.getKey(), mod.getSource(), hook);
-//					return false;
-//				}
-//				slot = new AvailableSlot(hook);
-//				model.addSlot(slot);
-//			}
-////			slot.addEmbeddedItem(accessory);
-
-			// Check if we already have this accessory - if yes, skip it
-			if (model.hasAccessory((ItemTemplate)accessory.getResolved(), mod.getId(), hook)) {
-				return true;
-			}
-			// Check if slot already has accessory - if no, add it
-//			if (!slot.hasAccessory((ItemTemplate)accessory.getResolved(), mod.getId(), hook)) {
-//				slot.addEmbeddedItem(accessory);
-//			}
-
 			SR6GearTool.recalculate("", charac, accessory);
 			//if (mod.isIncludedInStats())
-			boolean reallyAdded = model.addAccessory(accessory, hook);
-			if (!reallyAdded) {
-				return false;
+			// Check if AvailableSlot already exists - if not, create one
+			AvailableSlot slot = (AvailableSlot) model.getSlot(hook);
+			if (slot==null) {
+				if (hook.hasCapacity && strict) {
+					logger.log(Level.WARNING, "Item {0} (from {1}) has an <embed> for a not existing slot {2}, but cannot auto-create slot since no capacity is given",
+							mod.getKey(), mod.getSource(), hook);
+					return false;
+				}
+				slot = new AvailableSlot(hook);
+				model.addSlot(slot);
 			}
+			slot.addEmbeddedItem(accessory);
 
 			// Now apply modifications that the new accessory provides
 			for (Modification mod2 : accessory.getOutgoingModifications()) {
