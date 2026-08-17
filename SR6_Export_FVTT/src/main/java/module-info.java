ee494b10ea3ff144d769dd2569fdd4aa8e8cb997
Stefan Prelle <stefan@rpgframework.de>
2026-08-17 10:44:19 +0200

    Bundling all things Foundry, fixing module

--- SR6_Export_FVTT/src/main/java/module-info.java
+++ SR6_Export_FVTT/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module de.rpgframework.shadowrun6.export.fvtt  {
 	exports de.rpgframework.shadowrun6.export.fvtt;
+	exports de.rpgframework.shadowrun6.foundry;
+	exports de.rpgframework.foundry;
 	opens de.rpgframework.shadowrun6.export.fvtt to com.google.gson;
+	opens de.rpgframework.shadowrun6.foundry to com.google.gson;
+	opens de.rpgframework.foundry to com.google.gson;
 
 	requires de.rpgframework.core;
 	requires transitive de.rpgframework.rules;
